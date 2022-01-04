ef479b58f5450538f4a1fe4e5a4443f804a2721b
Karen Cubero Gonzalez <kcuberogonzalez@twilio.com>
2022-01-04 14:38:15 +0100

    feat(hooks): added git hooks to the project

---
+++ README.md
@@ -0,0 +1,5 @@
+APIs
+Country list: https://datahub.io/core/country-list#curl
+Twilio send SMS: https://www.twilio.com/docs/usage/webhooks/sms-webhooks
+Amadeus SDK: https://developers.amadeus.com/blog/get-started-amadeus-node-sdk
+Amadeus travel restrictions: https://developers.amadeus.com/self-service/category/covid-19-and-travel-safety/api-doc/travel-restrictions
--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-// Get country code from the body or get param (From Twilio SMS)
-const countryCode = "ES";
+const http = require("http");
+const express = require("express");
 
-// Get restrictions from Amadeus API
+const app = express();
+
 const Amadeus = require("amadeus");
 const amadeus = new Amadeus({
   clientId: process.env.AMADEUS_CLIENT_ID,
   clientSecret: process.env.AMADEUS_CLIENT_SECRET,
 });
 
-amadeus.client
-  .get("/v1/duty-of-care/diseases/covid19-area-report", { countryCode })
-  .then(function (response) {
-    console.log(response.data);
-    // Format message
+app.post("/sms", async (req, res) => {
+  // TODO: Get country code from the body or get param (From Twilio SMS)
+  const countryCode = "ES";
 
-    // Call Twilio to send SMS
-  })
-  .catch((error) => {
-    console.log("Error", error);
-  });
+  const restrictions = await amadeus.client.get(
+    "/v1/duty-of-care/diseases/covid19-area-report",
+    { countryCode }
+  );
+  console.log(`THE RESTRICTIONS FROM ${countryCode} ARE:\n
+  ${JSON.stringify(restrictions.data)}
+  `);
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end("<p>Worked</p>");
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log("Express server listening on port 1337");
+});
